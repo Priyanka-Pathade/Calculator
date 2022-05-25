76fd18537bd8332e47fab297ca4494a10a8f10f6
Priyanka-Pathade <priyankapathade2870@gmail.com>
2022-05-25 17:36:28 +0530

    All calculations added

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const sunIcon = "./assets/SunIcon.png";
 const moonIcon = "./assets/MoonIcon.png";
 const themeIcon = document.getElementById("theme-icon");
 const res = document.getElementById("result");
+const toast = document.getElementById("toast");
+
 // Displays entered value on screen.
 function liveScreen(enteredValue){
     if(!res.value){
@@ -11,3 +13,15 @@ function liveScreen(enteredValue){
     }
     res.value +=enteredValue;
 }
+
+function calculate(value) {
+    const calculatedValue = eval(value || null);
+    if (isNaN(calculatedValue)) {
+      res.value = "Can't divide 0 with 0";
+      setTimeout(() => {
+        res.value = "";
+      }, 1300);
+    } else {
+      res.value = calculatedValue;
+    }
+  }
