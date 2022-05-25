9b86cd6d81701399ea2d98cc3ba16a0c1ea4023e
Priyanka-Pathade <priyankapathade2870@gmail.com>
2022-05-25 18:18:20 +0530

    Keyboard inputs functionality added

--- script.js
+++ script.js
@@ -25,3 +25,66 @@ function calculate(value) {
       res.value = calculatedValue;
     }
   }
+
+  //adding event handler on the document to handle keyboard inputs
+    document.addEventListener("keydown", keyboardInputHandler);
+    //function to handle keyboard inputs
+    function keyboardInputHandler(e) {
+    // to fix the default behavior of browser,
+    // enter and backspace were causing undesired behavior when some key was already in focus.
+    e.preventDefault();
+    //grabbing the liveScreen
+  
+    //numbers
+    if (e.key === "0") {
+      res.value += "0";
+    } else if (e.key === "1") {
+      res.value += "1";
+    } else if (e.key === "2") {
+      res.value += "2";
+    } else if (e.key === "3") {
+      res.value += "3";
+    } else if (e.key === "4") {
+      res.value += "4";
+    } else if (e.key === "5") {
+      res.value += "5";
+    } else if (e.key === "6") {
+      res.value += "6";
+    } else if (e.key === "7") {
+      res.value += "7";
+    } else if (e.key === "7") {
+      res.value += "7";
+    } else if (e.key === "8") {
+      res.value += "8";
+    } else if (e.key === "9") {
+      res.value += "9";
+    }
+  
+    //operators
+    if (e.key === "+") {
+      res.value += "+";
+    } else if (e.key === "-") {
+      res.value += "-";
+    } else if (e.key === "*") {
+      res.value += "*";
+    } else if (e.key === "/") {
+      res.value += "/";
+    }
+  
+    //decimal key
+    if (e.key === ".") {
+      res.value += ".";
+    }
+
+    //press enter to see result
+    if (e.key === "Enter") {
+        calculate(result.value);
+    }
+
+    //backspace for removing the last input
+    if (e.key === "Backspace") {
+        const resultInput = res.value;
+        //remove the last element in the string
+        res.value = resultInput.substring(0, res.value.length - 1);
+  }
+}
